1ac38a351d1ad9dbbf61a8ed97132e4a5fdea894
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-26 18:43:06 +0530

    Update Home.svelte

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,8 +1,8 @@
-<!-- Home.svelte -->
+
 <script>
-    // Add any necessary logic or data for your home component
+   
   </script>
   
   <h1></h1>
-  <!-- Add your home page content here -->
+ 
   
