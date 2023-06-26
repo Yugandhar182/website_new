a944dd309e5f9b80e09ccc1af4d1547f7ff18392
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-26 16:11:13 +0530

    Update recruitingpricing.svelte

--- src/recruitingpricing.svelte
+++ src/recruitingpricing.svelte
@@ -1,107 +1,109 @@
 <script>
-    import 'bootstrap/dist/css/bootstrap.min.css';
-    </script>
-    
-    <section class="Recruitly Pricing">
-      <div class="pricing-component">
-        <div class="hero-section">
-          <h1 style="color:blue;">Recruitly Pricing</h1>
-          <p>Pricing that allows you to scale on demand</p>
-        </div>
-        <section class="features-section">
-          <table>
-          <div class="container">
-            <div class="col">
-              <div class="col-md-4">
-                <div class="feature-card">
-                  <i class="fa fa-search"></i>
-                  <h3 style="color:purple; font-weight:600;">solo</h3>
-                  <p>For independent recruiters</p>
-                  <h3 >$35</h3>
-                </div>
+   import 'bootstrap/dist/css/bootstrap.min.css';
+  </script>
+  
+  <section class="Recruitly Pricing">
+    <div class="pricing-component">
+      <div class="hero-section">
+        <h1 style="color:blue;">Recruitly Pricing</h1>
+        <p>Pricing that allows you to scale on demand</p>
+      </div>
+      <section class="features-section">
+        <table>
+        <div class="container">
+          <div class="col">
+            <div class="col-md-4">
+              <div class="feature-card">
+                <i class="fa fa-search"></i>
+                <h3 style="color:purple; font-weight:600;">solo</h3>
+                <p>For independent recruiters</p>
+                <h3 >$35</h3>
               </div>
-              <div class="col-md-4">
-                <div class="feature-card">
-                  <i class="fa fa-envelope"></i>
-                  <h3 style="color:purple; font-weight:600;">Startup</h3>
-                  <p>For startup agencies</p>
-                  <h3 >$50</h3>
-                </div>
+            </div>
+            <div class="col-md-4">
+              <div class="feature-card">
+                <i class="fa fa-envelope"></i>
+                <h3 style="color:purple; font-weight:600;">Startup</h3>
+                <p>For startup agencies</p>
+                <h3 >$50</h3>
               </div>
-              <div class="col-md-4">
-                <div class="feature-card">
-                  <i class="fa fa-users"></i>
-                  <h3 style="color:purple; font-weight:600;">Professional</h3>
-                  <p>For established teams</p>
-                  <h3 >$75</h3>
-                </div>
+            </div>
+            <div class="col-md-4">
+              <div class="feature-card">
+                <i class="fa fa-users"></i>
+                <h3 style="color:purple; font-weight:600;">Professional</h3>
+                <p>For established teams</p>
+                <h3 >$75</h3>
               </div>
             </div>
           </div>
-        </table>
-        </section>
-          
         </div>
+      </table>
+      </section>
         
-    </section>
-    
-    <style>
-        .hero-section {
-      text-align: center;
-      margin-bottom: 100px;
-      margin-top: -1000px;
-    }
-     
-    
-      .recruiting-pricing-section h1 {
-        color: blue;
-        font-weight: bold;
-        font-size: 5rem;
-      }
-    
-     
-      .close-button {
-        margin-top: 20px;
-        padding: 10px 20px;
-        background-color: purple;
-        color: white;
-        border: none;
-        border-radius: 5px;
-        font-size: 16px;
-        cursor: pointer;
-      }
-      body {
-        margin: 0;
-        padding: 0;
-        font-family: Arial, sans-serif;
-      }
-  
-      .pricing-component {
-        width: 100vh;
-        height: 250vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background-color: #f0f0f0;
-        padding: 20px;
-      }
-  
+      </div>
       
+  </section>
   
-      .pricing-component p {
-        font-size: 18px;
-        margin-bottom: 10px;
-      }
+  <style>
+      .hero-section {
+        text-align: center;
+    margin-top: -1000px;
+    margin-bottom: 10px;
+  }
+   
   
-      .pricing-component ul {
-        list-style-type: disc;
-        margin-left: 20px;
-      }
+    .recruiting-pricing-section h1 {
+      color: blue;
+      font-weight: bold;
+      font-size: 5rem;
+    }
   
-      .pricing-component li {
-        font-size: 16px;
-        margin-bottom: 5px;
-      }
-    </style>
+   
+    .close-button {
+      margin-top: 20px;
+      padding: 10px 20px;
+      background-color: purple;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      font-size: 16px;
+      cursor: pointer;
+    }
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: Arial, sans-serif;
+    }
+
+    .pricing-component {
+        width: 220vh;
+      height:250vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      background-color: #f0f0f0;
+      padding: 20px;
+      margin-left: 140vh;
+      margin-right: -50vh;
+    }
+
     
+
+    .pricing-component p {
+      font-size: 18px;
+      margin-bottom: 10px;
+    }
+
+    .pricing-component ul {
+      list-style-type: disc;
+      margin-left: 20px;
+    }
+
+    .pricing-component li {
+      font-size: 16px;
+      margin-bottom: 5px;
+    }
+  </style>
+  
