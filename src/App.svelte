fb5da48dd58d90f5de6efb55e783764bfced3782
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-26 18:41:56 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,53 +6,49 @@
 	import { Router, Link, Route } from "svelte-routing";
 	import RecruitingAutomation from "./recruitingautomation.svelte";
 	import RecruitingPricing from "./recruitingpricing.svelte";
-  
-  
-  
-	let showOverviewPopup = false;
-	let showWhyRecruitlyPopup = false;
+  import Popup from './Popup.svelte';
+  import Home from './Home.svelte';
+   let isPopupOpen = false;
+   let showOverviewPopup = false;
+  function openWhyRecruitlyPopup() {
+  isPopupOpen = true;
+    }
+  dispatch('openWhyRecruitlyPopup', { closePopup });
 
-  
-	let featureIndex = 0;
+  let featureIndex = 0;
 	const features = [
 	  { text: 'ATS', color: 'blue' },
 	  { text: 'CRM', color: 'green' },
 	  { text: 'Marketing', color: 'purple' }
 	];
-   
-  
-	
-  
-   
-	function openWhyRecruitlyPopup() {
-	  showWhyRecruitlyPopup = true;
-	}
-  
-	
-	function closePopup() {
-	 
-	  showWhyRecruitlyPopup = false;
-	  showOverviewPopup = false;
-	  
-	}
+
+     function closePopup() {
+     isPopupOpen = false;
+	    showOverviewPopup = false;
+	  }
+
+
 	function openOverview() {
-	 
-	  showOverviewPopup = true;
+	 showOverviewPopup = true;
 	}
+
 	setInterval(() => {
 	  featureIndex = (featureIndex + 1) % features.length;
 	}, 2000);
   
 	function navigateToAutomation() {
-    location.href = "/automation";
+    window.location.href =  "/automation";
 	}
   
 	function navigateToPricing() {
-    location.href="/pricing";
+    window.location.href ="/pricing";
 	}
    
   </script>
-  
+   {#if isPopupOpen}
+   <Popup {closePopup} />
+     {/if}
+   
   <main>
   
 	<div class="information">
@@ -162,41 +158,6 @@
 	
 	 </main>
   
-	 {#if showWhyRecruitlyPopup}
-	 
-	 
-	 <div class="popup" on:click={closePopup}>
-	  
-	   <div class="popup-content">
-		 <button class="close-button" on:click={closePopup}>&times;</button>
-		 <div class="popup-section">
-		   <h3>Email Marketing</h3>
-		   <p class="custom-paragraph">Engage talent at the right time, promote your business to the right audience.</p>
-		 </div>
-		 <div class="popup-section">
-		   <h3>Applicant Tracking System</h3>
-		   <p class="custom-paragraph">Candidate sharing, boolean searches, interview scheduling, automation and more.</p>
-		 </div>
-		 <div class="popup-section">
-		   <h3>Website Integration</h3>
-		   <p class="custom-paragraph">Turn your WordPress website into a fully featured job-board using our FREE plugins.</p>
-		 </div>
-		 <div class="popup-section">
-		   <h3>Job Distribution</h3>
-		   <p class="custom-paragraph">Get your job in front of millions of candidates, manage applicants in one place.</p>
-		 </div>
-		 <div class="popup-section">
-		   <h3>Search & Import</h3>
-		   <p class="custom-paragraph">Search and import candidates from CV-Library and TotalJobs with just a few clicks.</p>
-		 </div>
-		 <div class="popup-section">
-		   <h3>Recruiting Automation</h3>
-		   <p class="custom-paragraph">Boost your team's productivity by creating workflows that automate repetitive and common tasks.</p>
-		 </div>
-	   </div>
-	 </div>
-  
-   {/if}
    
   
   {#if showOverviewPopup}
@@ -241,7 +202,7 @@
   
 	.information {
 	  position: absolute;
-	  top: 50px;
+	  top: 18px;
 	  right: 270px;
 	  display: flex;
 	  flex-direction: row;
