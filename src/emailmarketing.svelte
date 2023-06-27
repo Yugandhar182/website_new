501cd2adc2ab2eee1ce46526b95f327f43fb95e6
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-27 21:04:47 +0530

    Create emailmarketing.svelte

---
+++ src/emailmarketing.svelte
@@ -0,0 +1,234 @@
+<script>
+	import emailmarketing from "./emailmarketing.svelte";
+	import { createEventDispatcher} from 'svelte';
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	import { navigate } from "svelte-routing";
+     const dispatch = createEventDispatcher();
+	 import { Router, Link, Route } from "svelte-routing";
+
+	
+	
+	export let closePopup;
+
+
+
+	function navigateToEmail() {
+	    window.location.href = "/Emailmarketing";
+	    }
+   </script>
+
+ <Router>
+  <div class="popup">
+	<h3>Why Recruitly?</h3>
+  
+	<div class="popup" on:click={closePopup}>
+	
+	  <div class="popup-content">
+		<button class="close-button" on:click={closePopup}>&times;</button>
+		<h1><Link to="/Emailmarketing" class="why-recruitly-anchor" style="color: blue; font-weight:700;">Email Marketing</Link></h1>
+		
+		<Route path="/Emailmarketing" component={emailmarketing} />
+		</div>
+	</div>
+   </div>
+ </Router>
+  <style>
+  
+  .popup {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+  }
+  
+  .popup-content {
+	position: relative;
+	background-color: #fff;
+	padding: 20px;
+	text-align: center;
+	border-radius: 5px;
+	width: 80%;
+	height: 80%;
+	max-width: 800px;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
+	overflow-y: auto;
+  }
+  
+  .popup-section {
+	width: calc(50% - 20px);
+	height: 25%;
+	padding: 10px;
+  }
+  
+  .custom-paragraph {
+	text-align: center;
+  }
+  
+  .popup-content h3 {
+	text-align: center;
+	color: blue;
+  }
+  
+  .popup-content p.custom-paragraph {
+	width: 100%;
+	text-align: center;
+  }
+  
+  .close-button {
+	position: absolute;
+	top: 0px;
+	right: 1px;
+	font-size: 24px;
+	color: #f00f22;
+	background: transparent;
+	border: none;
+	cursor: pointer;
+  }
+  .spacer {
+	margin: 0 10px;
+  }
+  
+	.container {
+	  text-align: center;
+	  margin-top: 200px;
+	}
+	h1.display-4 {
+	  font-size: 3.5rem;
+	  margin-bottom: 0.5rem;
+	  font-stretch: expanded;
+	}
+	.font-weight-bold {
+	  font-weight: bold;
+	}
+  
+	.header {
+	  background-color: #f8f9fa;
+	  padding: 20px;
+	  text-align: center;
+	}
+  
+	.information {
+	  position: absolute;
+	  top: 40px;
+	  right: 270px;
+	  display: flex;
+	  flex-direction: row;
+	  align-items: center;
+	}
+  
+	.information p {
+	  margin: 0;
+	  margin-left: 10px;
+	  cursor: pointer;
+	}
+  
+	.popup {
+	  position: fixed;
+	  top: 0;
+	  left: 0;
+	  width: 100%;
+	  height: 100%;
+	  background-color: rgba(0, 0, 0, 0.5);
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	}
+  
+	.popup-content {
+	  position: relative; /* Added position relative */
+	  background-color: #fff;
+	  padding: 20px;
+	  text-align: center;
+	  border-radius: 5px;
+	  width: 80%; /* Increased the width */
+	  height: 80%; /* Increased the height */
+	  max-width: 800px;
+	  display: flex;
+	  flex-wrap: wrap;
+	  justify-content: center;
+	  align-items: center;
+	  overflow-y: auto;
+	}
+  
+	.popup-section {
+	  width: calc(50% - 20px); 
+	  height: 25%; 
+	  padding: 10px;
+	}
+  
+	.custom-paragraph {
+	  text-align: center; 
+	}
+  
+	.popup-content h3 {
+	  text-align: center;
+	  color: blue;
+	}
+  
+	.popup-content p.custom-paragraph {
+	  width: 100%; /* Set the width for the paragraphs */
+	  text-align: center; /* Center the paragraph text */
+	}
+  
+	body {
+	  background-color: #e2f3fc;
+	}
+	.close-button {
+	  position: absolute;
+	  top: 0px;
+	  right: 1px;
+	  font-size: 24px;
+	  color: #f00f22;
+	  background: transparent;
+	  border: none;
+	  cursor: pointer;
+	}
+	.overview-button {
+	  margin-top: 10px;
+	  padding: 10px 20px;
+	  background-color: yellow;
+	  color: black;
+	  border: none;
+	  border-radius: 5px;
+	  font-size: 16px;
+	  cursor: pointer;
+	  display: flex;
+	  align-items: center;
+	  margin-left: 700px; /* Add margin from the left side */
+	  margin-right: auto; /* Add margin from the right side */
+	}
+	.popup-video-section {
+	  width: 100%;
+	  text-align: center;
+	}
+  
+	.popup-video {
+	  width: 100%;
+	  height: 400px;
+	}
+  
+	.why-recruitly-anchor {
+	  cursor: pointer;
+	}
+   
+	.modal {
+	  background-color: rgba(0, 0, 0, 0.5); /* Adjust the alpha value to control transparency */
+	}
+	img {
+    position: absolute;
+    top: 20px; /* Adjust the top position as needed */
+    left: 80px; /* Adjust the left position as needed */
+    width: 150px; /* Adjust the width as needed */
+    height: auto; /* Maintain aspect ratio */
+  }
+  
+  </style>
+  
