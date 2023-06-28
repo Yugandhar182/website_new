b2e1ad6da4a4c57d4fa542f9212dff907085c0dd
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-06-28 12:19:47 +0530

    Update emailmarketing.svelte

--- src/emailmarketing.svelte
+++ src/emailmarketing.svelte
@@ -1,234 +1,202 @@
 <script>
-	import emailmarketing from "./emailmarketing.svelte";
-	import { createEventDispatcher} from 'svelte';
-	import 'bootstrap/dist/css/bootstrap.min.css';
-	import { navigate } from "svelte-routing";
-     const dispatch = createEventDispatcher();
-	 import { Router, Link, Route } from "svelte-routing";
-
-	
-	
-	export let closePopup;
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import { Link } from "svelte-routing";
+  import { createEventDispatcher } from 'svelte';
+  import Popup from './Popup.svelte';
+ 
 
 
+  let isPopupOpen = false;
+  const dispatch = createEventDispatcher();
 
-	function navigateToEmail() {
-	    window.location.href = "/Emailmarketing";
-	    }
-   </script>
+  function openWhyRecruitlyPopup() {
+    isPopupOpen = true;
+  }
 
- <Router>
-  <div class="popup">
-	<h3>Why Recruitly?</h3>
-  
-	<div class="popup" on:click={closePopup}>
-	
-	  <div class="popup-content">
-		<button class="close-button" on:click={closePopup}>&times;</button>
-		<h1><Link to="/Emailmarketing" class="why-recruitly-anchor" style="color: blue; font-weight:700;">Email Marketing</Link></h1>
-		
-		<Route path="/Emailmarketing" component={emailmarketing} />
-		</div>
-	</div>
-   </div>
- </Router>
-  <style>
-  
-  .popup {
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background-color: rgba(0, 0, 0, 0.5);
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  function closePopup() {
+    isPopupOpen = false;
   }
-  
-  .popup-content {
-	position: relative;
-	background-color: #fff;
-	padding: 20px;
-	text-align: center;
-	border-radius: 5px;
-	width: 80%;
-	height: 80%;
-	max-width: 800px;
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: center;
-	align-items: center;
-	overflow-y: auto;
-  }
-  
-  .popup-section {
-	width: calc(50% - 20px);
-	height: 25%;
-	padding: 10px;
-  }
-  
-  .custom-paragraph {
-	text-align: center;
-  }
-  
-  .popup-content h3 {
-	text-align: center;
-	color: blue;
-  }
-  
-  .popup-content p.custom-paragraph {
-	width: 100%;
-	text-align: center;
-  }
-  
-  .close-button {
-	position: absolute;
-	top: 0px;
-	right: 1px;
-	font-size: 24px;
-	color: #f00f22;
-	background: transparent;
-	border: none;
-	cursor: pointer;
-  }
+
+  dispatch('openWhyRecruitlyPopup', { closePopup });
+</script>
+
+{#if isPopupOpen}
+  <Popup {closePopup} />
+{/if}
+
+<section class="recruiting-email-section">
+  <div class="links">
+    <nav>
+      <ul >
+        <a on:click={openWhyRecruitlyPopup} class="why-recruitly-anchor" style="color: blue; font-weight: 700;">Why Recruitly</a>
+        <span class="spacer"></span>
+        <li><Link to="/automation" style="color:blue; font-weight: 700;">Automation</Link></li>
+        <span class="spacer"></span>
+        <li><Link to="/pricing" style="color:blue; font-weight: 700;">Pricing</Link></li>
+      </ul>
+    </nav>
+  </div>
+  <div class="email-component">
+    <div class="hero-section">
+      <h2 style="color: darkmagenta;">Recruitment Email Marketing</h2>
+      <p style="color:black; font-weight: 700;">Reach your audience and improve your branding, with an email marketing solution </p>
+      <p style="color:black; font-weight: 600;">built right into  recruitment database.</p>
+    
+    </div>
+    <img src = https://recruitly.io/wp-content/uploads/2020/11/personalised-templates.png />
+    <section class="testimonials-section">
+      <table>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="feature-card">
+                <i class="fa fa-search"></i>
+                <h3 style="color:purple; font-weight:600;">Email Campaigns</h3>
+                <h6>Send e-shots directly from Recruitly, and create stunning campaigns for lead generation and engagement.
+              </h6>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="feature-card">
+                <i class="fa fa-envelope"></i>
+                <h3 style="color:purple; font-weight:600;">Email Templates</h3>
+                <h6>Create branded and re-usable email templates with our easy-to-use HTML editor.</h6>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="feature-card">
+                <i class="fa fa-users"></i>
+                <h3 style="color:purple; font-weight:600;">Distribution Lists</h3>
+                <h6>Create static distribution lists or link with one of your saved searches..</h6>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="feature-card">
+                <i class="fa fa-users"></i>
+                <h3 style="color:purple; font-weight:600;">Bounce Check</h3>
+                <h6>We safeguard your sender reputation by validating emails and checking for bounces before actually sending..</h6>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="feature-card">
+                <i class="fa fa-users"></i>
+                <h3 style="color:purple; font-weight:600;">Dedicated IP</h3>
+                <h6>You can opt for a dedicated IP address to take full control of emails sent from your IP to increase inbox placements.
+                  .</h6>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="feature-card">
+                <i class="fa fa-users"></i>
+                <h3 style="color:purple; font-weight:600;">Analytics</h3>
+                <h6>Provides analytics including applications, job boards, placements and many more that allow you to see which job board works better for your job adverts..</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </table>
+    </section>
+  </div>
+</section>
+
+<style>
   .spacer {
-	margin: 0 10px;
+    margin: 0 10px;
   }
-  
-	.container {
-	  text-align: center;
-	  margin-top: 200px;
-	}
-	h1.display-4 {
-	  font-size: 3.5rem;
-	  margin-bottom: 0.5rem;
-	  font-stretch: expanded;
-	}
-	.font-weight-bold {
-	  font-weight: bold;
-	}
-  
-	.header {
-	  background-color: #f8f9fa;
-	  padding: 20px;
-	  text-align: center;
-	}
-  
-	.information {
-	  position: absolute;
-	  top: 40px;
-	  right: 270px;
-	  display: flex;
-	  flex-direction: row;
-	  align-items: center;
-	}
-  
-	.information p {
-	  margin: 0;
-	  margin-left: 10px;
-	  cursor: pointer;
-	}
-  
-	.popup {
-	  position: fixed;
-	  top: 0;
-	  left: 0;
-	  width: 100%;
-	  height: 100%;
-	  background-color: rgba(0, 0, 0, 0.5);
-	  display: flex;
-	  justify-content: center;
-	  align-items: center;
-	}
-  
-	.popup-content {
-	  position: relative; /* Added position relative */
-	  background-color: #fff;
-	  padding: 20px;
-	  text-align: center;
-	  border-radius: 5px;
-	  width: 80%; /* Increased the width */
-	  height: 80%; /* Increased the height */
-	  max-width: 800px;
-	  display: flex;
-	  flex-wrap: wrap;
-	  justify-content: center;
-	  align-items: center;
-	  overflow-y: auto;
-	}
-  
-	.popup-section {
-	  width: calc(50% - 20px); 
-	  height: 25%; 
-	  padding: 10px;
-	}
-  
-	.custom-paragraph {
-	  text-align: center; 
-	}
-  
-	.popup-content h3 {
-	  text-align: center;
-	  color: blue;
-	}
-  
-	.popup-content p.custom-paragraph {
-	  width: 100%; /* Set the width for the paragraphs */
-	  text-align: center; /* Center the paragraph text */
-	}
-  
-	body {
-	  background-color: #e2f3fc;
-	}
-	.close-button {
-	  position: absolute;
-	  top: 0px;
-	  right: 1px;
-	  font-size: 24px;
-	  color: #f00f22;
-	  background: transparent;
-	  border: none;
-	  cursor: pointer;
-	}
-	.overview-button {
-	  margin-top: 10px;
-	  padding: 10px 20px;
-	  background-color: yellow;
-	  color: black;
-	  border: none;
-	  border-radius: 5px;
-	  font-size: 16px;
-	  cursor: pointer;
-	  display: flex;
-	  align-items: center;
-	  margin-left: 700px; /* Add margin from the left side */
-	  margin-right: auto; /* Add margin from the right side */
-	}
-	.popup-video-section {
-	  width: 100%;
-	  text-align: center;
-	}
-  
-	.popup-video {
-	  width: 100%;
-	  height: 400px;
-	}
-  
-	.why-recruitly-anchor {
-	  cursor: pointer;
-	}
+  .hero-section {
+  text-align: center;
+  margin-top: -600px;
+  margin-bottom: 10px;
+}
+.recruiting-email-section h{
    
-	.modal {
-	  background-color: rgba(0, 0, 0, 0.5); /* Adjust the alpha value to control transparency */
-	}
-	img {
-    position: absolute;
-    top: 20px; /* Adjust the top position as needed */
-    left: 80px; /* Adjust the left position as needed */
-    width: 150px; /* Adjust the width as needed */
-    height: auto; /* Maintain aspect ratio */
+   font-weight: bold;
+   font-size: 2rem;
+   margin-top: 80px;
+ }
+  .recruiting-email-section h2 {
+      margin-top: 1px;
+  margin-bottom: 1px;
+    font-weight: bold;
+    font-size: 3rem;
+  }
+  .recruiting-email-section p {
+   
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+
+  .recruiting-email-section h3 {
+
+    font-weight: bold;
+    font-size: 1.7rem;
+    margin-top: 50px;
+  }
+  .recruiting-email-section h6 {
+
+  font-weight: bold;
+  font-size: 1rem;
+  margin-top: 10px;
+ }
+
+
+
+ 
+  
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, sans-serif;
+  }
+
+  .email-component {
+     margin-top:5vh;
+    width: 215vh;
+    height:250vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color:white;
+    padding: 20px;
+    margin-left: 140vh;
+    margin-right: -50vh;
+  }
+
+  .why-recruitly-anchor {
+  cursor: pointer;
+}
+ 
+
+ 
+
+  .automation-component ul {
+    list-style-type: disc;
+    margin-left: 20px;
+  }
+
+  .automation-component li {
+    font-size: 10px;
+    margin-bottom: 5px;
   }
   
-  </style>
-  
+  .links {
+  display: flex;
+  justify-content: flex-end;
+  position: absolute;
+  top:8px;
+  right: 10px;
+}
+
+.links ul {
+  display: flex;
+  list-style-type: none;
+  padding: 0;
+}
+
+.links li {
+  margin-right: 0px;
+}
+
+ 
+</style>
